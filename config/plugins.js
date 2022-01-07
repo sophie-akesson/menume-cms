e66e1f3fa424511b1e940e3e685622923d2d6951
Sophie Åkesson <sophie.akesson@gmail.com>
2022-01-07 14:51:51 +0100

    Fix forgot password

--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   email: {
     provider: "nodemailer",
     providerOptions: {
-      host: env("SMTP_HOST", "smtp.example.com"),
+      host: env("SMTP_HOST", "smtp.gmail.com"),
       port: env("SMTP_PORT", 587),
       auth: {
         user: env("SMTP_USERNAME"),
